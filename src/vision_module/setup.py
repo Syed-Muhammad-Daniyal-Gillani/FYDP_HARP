2aa4f918dbef77eda5385ee4da65ffb07c93674f
darkdev <darkdev>
2025-02-22 16:01:45 +0500

    fix - removed hard coded face cascade path

--- src/vision_module/setup.py
+++ src/vision_module/setup.py
@@ -8,16 +8,16 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+            ['resource/' + package_name]),  # Keeps package metadata
+        ('share/' + package_name, ['package.xml']),  # Ensures package.xml is installed
+        ('share/' + package_name + '/resource', ['resource/haarcascade_frontalface_default.xml']),  # INSTALLS haarcascade!
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='darkdev',
     maintainer_email='darkdev@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    # tests_require=['pytest'],
+    description='Face tracking module for ROS2',
+    license='MIT',
     entry_points={
         'console_scripts': [
             'face_tracker = vision_module.main:main',
