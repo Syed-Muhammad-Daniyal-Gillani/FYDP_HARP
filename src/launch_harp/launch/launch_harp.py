c604932c5659b521e518b58b05fd3bbc57d40d09
DarkDev <DarkDev@Dell>
2025-02-27 18:14:09 +0500

    INit, created a launch file to run multiple modules at the same time

---
+++ src/launch_harp/launch/launch_harp.py
@@ -0,0 +1,41 @@
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+import os
+def generate_launch_description():
+    return LaunchDescription([
+
+        Node(
+            package='vision_module',
+            executable='face_tracker',
+            name='Vision',
+            output = 'screen'
+        ),
+
+        Node(
+            package='face_animations',
+            executable='face_animations',
+            name='Eyes_Display',
+            output = 'screen'
+        ),
+        Node(
+            package='neck_module',
+            executable='neck_controller',
+            name='Neck',
+            output = 'screen'
+        ),
+        # Node(
+        #     package='rosbridge_server',
+        #     executable='rosbridge_websocket',
+        #     name='ROSBridge_Local_Server',
+        #     output = 'screen'            
+        # )
+        # Rosbridge WebSocket (Runs Outside Venv)
+        # rosbridge_server runs outside venv
+        # ExecuteProcess(
+        #     cmd=['/opt/ros/humble', '-m', 'rosbridge_server.rosbridge_websocket'],
+        #     output='screen'
+        # )
+
+    ])
